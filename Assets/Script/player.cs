e7cc2e390662d0e05d2816fe09b0c00a6df97b9b
johannn29 <johannnesxiimipa315@gmail.com>
2026-02-23 22:57:39 +0700

    commit 4
    hubungin kabel mekanik

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -1,66 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class AU_PlayerController : MonoBehaviour
 {
-    // Components
-    Rigidbody myRB;
-    Transform myAvatar;
-
-    // Player movement
-    [SerializeField] InputAction WASD;
+    Rigidbody2D myRB;
     Vector2 movementInput;
+    float speed = 5f;
 
-    [SerializeField] float baseSpeed = 5f;       // kecepatan awal
-    [SerializeField] float maxSpeed = 12f;       // batas maksimal
-    [SerializeField] float acceleration = 0.2f;  // seberapa cepat nambah speed
+    InputAction moveAction;
 
-    float currentSpeed;
-
-    private void OnEnable()
+    void Awake()
     {
-        WASD.Enable();
+        moveAction = new InputAction(type: InputActionType.Value, expectedControlType: "Vector2");
+        moveAction.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
     }
 
-    private void OnDisable()
-    {
-        WASD.Disable();
-    }
+    void OnEnable() => moveAction.Enable();
+    void OnDisable() => moveAction.Disable();
 
     void Start()
     {
-        myRB = GetComponent<Rigidbody>();
-        myAvatar = transform.GetChild(0);
-        currentSpeed = baseSpeed;
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        movementInput = WASD.ReadValue<Vector2>();
-
-        // Flip avatar sesuai arah X
-        if (movementInput.x != 0)
-        {
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
-        }
-
-        // Jika ada input, tambah speed sampai max
-        if (movementInput != Vector2.zero)
-        {
-            currentSpeed = Mathf.Min(currentSpeed + acceleration, maxSpeed);
-        }
-        else
-        {
-            // Kalau tidak ada input, reset ke baseSpeed
-            currentSpeed = baseSpeed;
-        }
+        movementInput = moveAction.ReadValue<Vector2>();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        myRB.linearVelocity = movementInput * currentSpeed;
+        if (myRB != null)
+            myRB.linearVelocity = movementInput * speed;
     }
 }
